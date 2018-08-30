3714ac0ff39aff2be7b2b19504778ecbb1dbe460
AnniePawl <annampawl@gmail.com>
2018-08-30 14:24:24 -0700

    added reviews-index.handlebars

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
+// INDEX
+app.get('/reviews', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
