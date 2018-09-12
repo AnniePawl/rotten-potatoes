8a876d2ecf4201d668a57e075c63ab8c493f46a8
Anna Pawl <annampawl@gmail.com>
2018-09-11 17:05:41 -0700

    test show

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -77,3 +77,17 @@ it('should create a SINGLE review on /reviews POST', (done) =>{
         done();
     });
 });
+
+// TEST SHOW
+ it('should create a SINGLE review on /reviews/<id> GET', (done) => {
+   var review = new Review(sampleReview);
+   review.save((err, data) =>{
+       chai.request('http://localhost:3000')
+           .get(`/reviews/${data._id}`)
+           .end((err, res) =>{
+               res.should.have.status(200);
+               res.should.be.html
+               done();
+           });
+   });
+});
